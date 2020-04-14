f6e5bda82a5996eec7d03ec7ebf865397bb8fe1b
Michael Dlugosch <michael.dlugosch@web.de>
2020-04-14 10:03:36 +0200

    Dijkstra

---
+++ DijkstraAlgo/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DijkstraAlgo
+{
+    public class Node
+    {
+        public string Name { get; set; }
+        public double Costs { get; set; }
+        public Node Parent { get; set; }
+    }
+}
